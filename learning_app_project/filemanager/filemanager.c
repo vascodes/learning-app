739a2fe1cab780326c2bd980a8f82983a4c78391
vascodes <nikhilvas12@gmail.com>
2022-10-21 20:56:52 +0530

    Add append_line_to_end function to filemanager.c

--- learning_app_project/filemanager/filemanager.c
+++ learning_app_project/filemanager/filemanager.c
@@ -20,3 +20,16 @@ int delete_file(char *filename){
 	
 	return is_deleted;
 }
+
+int append_line_to_end(char *filename, char *str){
+	int is_file_exists;
+	file_ptr = fopen(filename, "a");
+	
+	if(file_ptr != NULL){
+		is_file_exists = 1;
+		fprintf(file_ptr, str);
+	}
+
+	fclose(file_ptr);
+	return is_file_exists;
+}
--- learning_app_project/filemanager/filemanager.h
+++ learning_app_project/filemanager/filemanager.h
@@ -3,5 +3,6 @@
 
 int create_file(char *);
 int delete_file(char *);
+int append_line_to_end(char *, char *);
 
 #endif
