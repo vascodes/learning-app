e4f0e4be16e33d0f9a86463524012456ec95fc39
vascodes <nikhilvas12@gmail.com>
2022-10-22 05:48:50 +0530

    Add get_line_by_number function to filemanager.

--- learning_app_project/filemanager/filemanager.c
+++ learning_app_project/filemanager/filemanager.c
@@ -1,8 +1,14 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h> 
 #include "filemanager.h"
 
 FILE *file_ptr;
 
+/*
+	Creates a file with given file name.
+	Returns 1 if file is created else 0.
+*/
 int create_file(char *filename){
 	int is_created;
 	
@@ -14,6 +20,10 @@ int create_file(char *filename){
 	return is_created;	
 }
 
+/*
+	Deletes a file with given file name.
+	Returns 1 if file is deleted else 0.
+*/
 int delete_file(char *filename){
 	int is_deleted;
 	is_deleted = remove(filename);	
@@ -21,15 +31,53 @@ int delete_file(char *filename){
 	return is_deleted;
 }
 
-int append_line_to_end(char *filename, char *str){
-	int is_file_exists;
-	file_ptr = fopen(filename, "a");
+/*
+	To append a line to end of specified file.
+	filename - Name of file
+	str - String that is to be appended. 		
 	
-	if(file_ptr != NULL){
-		is_file_exists = 1;
-		fprintf(file_ptr, str);
+	Returns 1 if line is appended else 0.
+*/
+int append_line_to_end(char *filename, char str[100]){
+	int is_str_appended;	
+	file_ptr = fopen(filename, "a");	
+			
+	if(file_ptr == NULL){
+		printf("\nError! Could not open file.");
+		return 0;
 	}
 
+	is_str_appended = fputs(str, file_ptr);	
+	
 	fclose(file_ptr);
-	return is_file_exists;
+	return is_str_appended;
+}
+
+/*
+	To get a specified line using line number from a file.	
+	file_name: Name of file.
+	line_num: 	Line number of line to be read from file.
+	
+	Returns line read from file.					
+*/
+char *get_line_by_number(char *filename, int line_num){	
+	int is_file_exists;
+	int curr_line = 1, max_len = 150;
+	char *str = (char *) malloc(max_len * sizeof(char));
+
+	file_ptr = fopen(filename, "r");			
+	if(file_ptr == NULL)
+		printf("\nError. Could not open file %s\n.", filename);
+	else{
+		while(fgets(str, max_len, file_ptr)){			
+			if(curr_line == line_num)
+				break;
+							
+			curr_line++;			
+		}
+		printf("\nstring is: %s\n", str);
+	}
+	
+	fclose(file_ptr);
+	return str;
 }
--- learning_app_project/filemanager/filemanager.h
+++ learning_app_project/filemanager/filemanager.h
@@ -3,6 +3,7 @@
 
 int create_file(char *);
 int delete_file(char *);
-int append_line_to_end(char *, char *);
+int append_line_to_end(char *, char str[]);
+char *get_line_by_number(char *, int);
 
 #endif
