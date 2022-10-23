7bad91f9d2f922025e675caac75e1869a9650af7
vascodes <nikhilvas12@gmail.com>
2022-10-24 02:09:56 +0530

    Add queues to questionmanager.c

--- learning_app_project/main.c
+++ learning_app_project/main.c
@@ -4,15 +4,8 @@
 int main() {				
 	int i;
 	question questions[100];
-	qm_create_questions();
-	qm_set_questions_arr();
-	int n = qm_get_all_questions(questions);
-	printf("\n");
-	for(i = 0; i < n; i++){
-		printf("Question: %s\n", questions[i].question);
-		printf("Answer: %s\n", questions[i].answer);
-		printf("Frequency: %d\n\n", questions[i].frequency);
-	}
+//	qm_create_questions();
+	qm_start();
 	
 	return 0;
 }
