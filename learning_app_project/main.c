e40d34b10c6799d5146673c4704198c35437243e
vascodes <nikhilvas12@gmail.com>
2022-10-23 17:11:26 +0530

    Add qm_get_question_from_str function to questionmanager.c

--- learning_app_project/main.c
+++ learning_app_project/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "questionmanager/questionmanager.h"
 
-int main() {		
+int main() {			
+	question q;
+	char str[] = "what is german for one?|eins|2";
+	qm_get_question_from_str(str, &q);
+	
+	printf("\nThe question is: %s", q.question);
+	printf("\nThe answer is: %s", q.answer);
+	printf("\nThe freq is: %d", q.frequency);
+	
 	return 0;
 }
--- learning_app_project/questionmanager/questionmanager.c
+++ learning_app_project/questionmanager/questionmanager.c
@@ -6,14 +6,10 @@
 
 #define DEFAULT_FREQ 2
 #define DEFAULT_FILENAME "qalist.txt"
-
-typedef struct qm_question{
-    int frequency;
-    char question[200];
-    char answer[200];
-} question;
+#define DEFAULT_DELIMITER "|"
 
 char QUESTION_FORMAT_STR[20] = "%s|%s|%d\n";
+
 
 void *qm_get_question_str(char *question, char *answer, int freq, char out_str[]){
 	int max_len = 200;
@@ -58,3 +54,33 @@ void qm_create_questions(int frequency){
 		qm_add_question_from_user();
 	}
 }
+
+int qm_get_question_from_str(char question_str[200], question *out_question){
+	int i, index = 0, question_str_len;
+	char *str;		
+	char str_split_arr[5][strlen(question_str)];
+	
+	/*
+		Split contents of a string with delimiter to an array.
+		
+		example: 	If question_str = "Hello|World|10", then
+					str_split_arr will be ["Hello", "World", "10"]
+	*/	
+	str = strtok(question_str, DEFAULT_DELIMITER);
+	while(str != NULL){		
+		strcpy(str_split_arr[index], str);
+		index++;		
+		str = strtok(NULL, DEFAULT_DELIMITER);
+	}	
+	
+	// Map question, answer and frequency to a question type.
+	strcpy(out_question->question, str_split_arr[0]);
+	strcpy(out_question->answer, str_split_arr[1]);
+	out_question->frequency =  atoi(str_split_arr[2]);
+	
+	return;	
+}
+
+
+
+
